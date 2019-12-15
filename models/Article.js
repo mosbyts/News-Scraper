7cb6dd574aaddbe7f71aa96ca9a8e83341ce7119
Tianna Mosby <mosbyts@vcu.edu>
2019-12-15 18:56:53 -0500

    Almost full functionality, comments value not working properly.

--- models/Article.js
+++ models/Article.js
@@ -20,7 +20,7 @@ var ArticleSchema = new Schema ({
     },
     comment: {
         type: Schema.Types.ObjectId,
-        ref: "Comment"
+        ref: "comment"
     }
 });
 
--- public/assets/js/news.js
+++ public/assets/js/news.js
@@ -1,7 +1,7 @@
 // Whenever someone clicks a news div
   $(document).on("click", ".newsStory", function() {
-    // Empty the notes from the note section
-    $("#notes").empty();
+    // Empty the comments from the note section
+    $("#comments").empty();
     // Save the id from the news div
     var thisId = $(this).attr("data-id");
     // Now make an ajax call for the Article
@@ -12,47 +12,46 @@
       // With that done, add the note information to the page
       .then(function(data) {
         // The title of the article
-        $("#notes").append("<h3>" + data.headline + "</h3>");
+        $("#comments").append("<h3>" + data.headline + "</h3>");
         // An input to enter a new title
-        $("#notes").append("<input id='titleinput' name='title' >");
+        $("#comments").append("<p>Name: </p><input id='titleinput' name='name' >");
         // A textarea to add a new note body
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+        $("#comments").append("<p>Comment: </p><textarea id='bodyinput' name='comment'></textarea>");
         // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
+        $("#comments").append("<button data-id='" + data._id + "' id='saveComment'>Save Comment</button>");
         // If there's a comment in the article
         if (data.comment) {
           // Place the title of the note in the title input
-          $("#titleinput").val(data.comment.title);
+          $("#titleinput").val(data.comment.name);
           // Place the body of the note in the body textarea
-          $("#bodyinput").val(data.comment.body);
+          $("#bodyinput").val(data.comment.comment);
         }
       });
   });
   
   // When you click the savenote button
-  $(document).on("click", "#savenote", function() {
+  $(document).on("click", "#saveComment", function() {
     // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
-  
+    console.log(thisId);
     // Run a POST request to change the note, using what's entered in the inputs
     $.ajax({
       method: "POST",
       url: "/" + thisId,
       data: {
         // Value taken from title input
-        title: $("#titleinput").val(),
+        name: $("#titleinput").val(),
         // Value taken from note textarea
-        body: $("#bodyinput").val()
+        comment: $("#bodyinput").val()
       }
     })
       // With that done
       .then(function(data) {
         // Log the response
-        console.log("Note title: " + data.title);
-        console.log("Note body: " + data.body);
-        // Empty the notes section
-        //$("#notes").empty();
+        console.log("Comment title: " + data.name);
+        console.log("Comment body: " + data.comment);
+        // Empty the comments section
+        //$("#comments").empty();
       });
   
     // Also, remove the values entered in the input and textarea for note entry
